1bc83fe283859e9d14691afd2f16f8179f61fc16
sowmiya <31585889+sowmiyasuba@users.noreply.github.com>
2021-11-20 21:43:30 -0800

    beautiful_dosa.java

---
+++ beautiful_dosa.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+public class Beautiful_dosa {
+	
+	int onion=2;
+	int chilli=5;
+	
+	int coriander=10;
+	
+	
+	
+	public void Dosa_spcl()
+	{
+		if(onion==2)
+		{
+			System.out.println("1)chop " + onion +" onion  into small pieces");
+			System.out.println();
+		}
+		if(chilli==5)
+		{
+			System.out.println("2)chop "+ chilli +" chilli into long pieces");
+			System.out.println();
+		}
+		
+		if(coriander==10)
+		{
+			System.out.println("3)chop "+ coriander +" coriander into long pieces");
+			System.out.println();
+		}
+		
+		if(onion==2&chilli==5&coriander==10)
+		{
+			System.out.println("4)pour into dosa flour and starting preparing your favourite roast dosa");
+			System.out.println();
+			
+		}
+		
+		System.out.println("5)wait for 2 minutes to complete.one done you are good to server it");
+		System.out.println();
+		
+		
+	}
+
+}
